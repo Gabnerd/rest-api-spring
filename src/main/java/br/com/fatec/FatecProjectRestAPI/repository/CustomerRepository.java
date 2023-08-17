547de1bd7f493acf37fe09bcb52cd073c3c917f4
Gabriel Pescaroli <ShaickMiles@gmail.com>
2023-08-16 22:23:45 -0300

    repository customer alter

--- src/main/java/br/com/fatec/FatecProjectRestAPI/repository/CustomerRepository.java
+++ src/main/java/br/com/fatec/FatecProjectRestAPI/repository/CustomerRepository.java
@@ -3,7 +3,7 @@ package br.com.fatec.FatecProjectRestAPI.repository;
 import br.com.fatec.FatecProjectRestAPI.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+//repository Customer
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }

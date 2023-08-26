b10b6fa76b6ef250279de12a89acfeaf28e866e0
Gabnerd <ShaickMiles@gmail.com>
2023-08-26 15:57:23 -0300

    feat: implement list product

---
+++ src/main/java/br/com/fatec/FatecProjectRestAPI/controller/ProductController.java
@@ -0,0 +1,25 @@
+package br.com.fatec.FatecProjectRestAPI.controller;
+
+import br.com.fatec.FatecProjectRestAPI.entity.Product;
+import br.com.fatec.FatecProjectRestAPI.exception.ResponseGenericException;
+import br.com.fatec.FatecProjectRestAPI.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/v1/product")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping(value = "list")
+    public ResponseEntity<Object> list(){
+        List<Product> result = productService.getProducts();
+        return ResponseEntity.ok().body(ResponseGenericException.response(result));
+    }
+}
